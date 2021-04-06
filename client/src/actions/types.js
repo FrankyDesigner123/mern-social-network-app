8b55d4da00c2eb32ae29c4b3dd049485f01f4b41
Franky <62474988+ninja-uiux@users.noreply.github.com>
2021-04-06 23:04:12 +0200

    Profile Reducers & Get current profile
    - Create profileReducer and add it to rootReducer
    - Create GET_PROFILE and PROFILE_ERROR in action/types.js
    - getCurrentProfile function in action/profile.js
    - dispatch the actions to the reducer
    - connect  profilereducers to componentstate "Dashboard"

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+
+// Auth - Login
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 
@@ -10,3 +12,7 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 
 export const LOGOUT = 'LOGOUT';
+
+// Profile
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
