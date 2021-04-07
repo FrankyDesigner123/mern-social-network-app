1a7c179651994dea8e4f0eb1fb04de73dc3fcc7c
Franky <62474988+ninja-uiux@users.noreply.github.com>
2021-04-07 09:25:07 +0200

    CreateProfile component
    - Create profile-form component(HTML) and add it into App.js
    - Toggle social network (dropdown) using toggleState
    - add onChange eventhandler on every input state

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 import Alert from './components/layout/Alert';
 
@@ -39,6 +40,11 @@ const App = () => {
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
 							<PrivateRoute exact path="/dashboard" component={Dashboard} />
+							<PrivateRoute
+								exact
+								path="/create-profile"
+								component={CreateProfile}
+							/>
 						</Switch>
 					</section>
 				</Fragment>
