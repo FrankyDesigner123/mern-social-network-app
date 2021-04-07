b28c0027e2ca16a95a2fe91d910113f26d360752
Franky <62474988+ninja-uiux@users.noreply.github.com>
2021-04-07 14:43:53 +0200

    Delete education, experiences & Account
    - deleteExperience(), deleteEducation(), deleteAccount() actions in action/profile.js
    - Connect reducer actions to component state
    - DeleteAccount button in dashboard.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,7 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-const { response } = require('express');
+const Post = require('../../models/Post');
 
 // @route       GET api/profile/me
 // @desc        Get current users profile
@@ -154,7 +154,7 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
 	try {
 		// remove users posts
-
+		await Post.deleteMany({ user: req.user.id });
 		// remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 		// remove user
